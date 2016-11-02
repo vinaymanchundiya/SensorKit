2657d591ee14a333b79fdde1fd6e123813b29086
andreuke <andreatello92@gmail.com>
2016-11-02 19:58:37 +0100

    Initial Commit

---
+++ SensorKit/SensorKit.h
@@ -0,0 +1,19 @@
+//
+//  SensorKit.h
+//  SensorKit
+//
+//  Created by Andrea Piscitello on 02/11/16.
+//  Copyright © 2016 Andrea Piscitello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SensorKit.
+FOUNDATION_EXPORT double SensorKitVersionNumber;
+
+//! Project version string for SensorKit.
+FOUNDATION_EXPORT const unsigned char SensorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SensorKit/PublicHeader.h>
+
+
